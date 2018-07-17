348b21723fb8582d12805389fbebe6d2259bfb6d
Dan Allison <daniel.j.allison@gmail.com>
2018-07-16 20:22:29 -0700

    Add md5 hash for data versioning

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from hashlib import md5
 
 def fetch_phishtank_csv():
     '''
@@ -7,6 +8,17 @@ def fetch_phishtank_csv():
     '''
     api_key = os.environ['PHISHTANK_API_KEY']
     url = f'https://data.phishtank.com/data/{api_key}/online-valid.csv.gz'
+
+    print('fetching file from phishtank.com')
     response = requests.get(url)
-    with open('/usr/src/app/data/phishtank.csv.gz', 'wb') as file:
+
+    # Phishtank updates their data every hour, so we include
+    # in the filename the md5 hash of the contents as a way
+    # of versioning the data.
+    file_hash = md5(response.content).hexdigest()
+    file_path = f'/usr/src/app/data/phishtank-{file_hash}.csv.gz'
+
+    with open(file_path, 'wb') as file:
+        print(f'saving file to {file_path}')
         file.write(response.content)
+    print('done')
